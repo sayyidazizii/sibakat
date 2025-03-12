5defddf4294ed684df32484ebb3e2c5c09913024
sayyidazizii <sayyidsyafiq234@gmail.com>
2025-03-12 12:28:49 +0700

    enhance(Auth_Providers): checkLoginStatus if loggedIn create notifikasi

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 import '../models/user_model.dart';
 import '../services/api_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class AuthProvider extends ChangeNotifier {
   User? _user;
   bool _isLoading = false;
+  bool _isLoggedIn = false; // Tambahkan variabel untuk sesi login
 
   User? get user => _user;
   bool get isLoading => _isLoading;
+  bool get isLoggedIn => _isLoggedIn; // Getter untuk status login
 
   final ApiService _apiService = ApiService();
 
+  //* Fungsi untuk mengecek status login
+  Future<void> checkLoginStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    if (token != null) {
+      _isLoggedIn = true;
+      notifyListeners();
+      //* Tampilkan notifikasi selamat datang kembali
+      Fluttertoast.showToast(
+        msg: "Selamat datang kembali!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.TOP, // Notifikasi di atas
+        backgroundColor: Colors.black54,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    }
+  }
+
+  //* Fungsi untuk Login
   Future<bool> login(String email, String password) async {
     _isLoading = true;
     notifyListeners();
@@ -19,6 +44,7 @@ class AuthProvider extends ChangeNotifier {
 
     if (loggedInUser != null) {
       _user = loggedInUser;
+      _isLoggedIn = true; // Set status login 
       _isLoading = false;
       notifyListeners();
       return true; // Login berhasil
@@ -27,5 +53,14 @@ class AuthProvider extends ChangeNotifier {
       notifyListeners();
       return false; // Login gagal
     }
+  }
+
+  //* Fungsi untuk logout
+  Future<void> logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+    _isLoggedIn = false; // Reset status login
+    _user = null;
+    notifyListeners();
   }
 }
