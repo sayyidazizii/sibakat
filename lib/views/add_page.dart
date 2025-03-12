c55b5185faba2ed3b204b3fd788b0354f2b35c1a
sayyidazizii <sayyidsyafiq234@gmail.com>
2025-03-12 12:30:02 +0700

    feat(AddPage): create add page

---
+++ lib/views/add_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Tambah Kartu')),
+      body: Center(child: Text('Halaman Tambah Kartu')),
+    );
+  }
+}
